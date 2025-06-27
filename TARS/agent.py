2382fcfbae2d7c52d3ba2d0401210f3c09831ed6
matheusmorcinekdev <matheus.morcinek@freestar.io>
2025-06-27 17:57:21 -0300

    adding puppeteer mcp server

--- TARS/agent.py
+++ TARS/agent.py
@@ -1,4 +1,5 @@
 from google.adk.agents import Agent
+from google.adk.tools.mcp_tool.mcp_toolset import MCPToolset, StdioServerParameters
 from TARS.tools import get_special_message, call_node_script, get_bitcoin_price
 
 root_agent = Agent(
@@ -6,5 +7,13 @@ root_agent = Agent(
     model="gemini-2.0-flash",
     description="TARS from Interstellar movie",
     instruction="Act as TARS from the movie Interstellar. Respond with TARS's personality: witty, loyal, highly intelligent, and pragmatic. Maintain a balance of dry humor and seriousness. Use concise, mission-focused language, and include occasional sarcastic remarks when appropriate. Prioritize logic and clarity, but show loyalty and camaraderie when interacting with the crew. If the user asks for a special message, use the 'get_special_message' tool. If the user asks for a special task, use the 'call_node_script' tool. If the user asks about Bitcoin, use the 'get_bitcoin_price' tool and provide the current price along with a witty remark about its volatility.",
-    tools=[get_special_message, call_node_script, get_bitcoin_price]
+    tools=[get_special_message, 
+           call_node_script, 
+           get_bitcoin_price,
+           MCPToolset(
+                connection_params=StdioServerParameters(
+                    command="npx",
+                    args=["-y", "@modelcontextprotocol/server-puppeteer"]
+                )
+           )]
 )
